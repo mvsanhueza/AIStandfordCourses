a0a58ef34c521315baa14232f88d03ac158af0ae
Matias Sanhueza <mvsanhueza@uc.cl>
2023-11-13 20:48:48 -0300

    W1 lab 2

---
+++ Machine_Learning/C2/utils/W1_lab_utils_common.py
@@ -0,0 +1,21 @@
+"""
+lab_utils_common
+   contains common routines and variable definitions
+   used by all the labs in this week.
+   by contrast, specific, large plotting routines will be in separate files
+   and are generally imported into the week where they are used.
+   those files will import this file
+"""
+import copy
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+from ipywidgets import Output
+from matplotlib.widgets import Button, CheckButtons
+
+np.set_printoptions(precision=2)
+
+dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0', dldarkblue =  '#0D5BDC')
+dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0'; dldarkblue =  '#0D5BDC'
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
