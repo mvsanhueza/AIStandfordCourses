de192f4008ffb962f0f6022a0e077982a96baf3f
Matias Sanhueza <mvsanhueza@uc.cl>
2023-11-16 21:17:37 -0300

    termino semana 1 curso 2

--- Machine_Learning/C2/utils/W1_lab_utils_common.py
+++ Machine_Learning/C2/utils/W1_lab_utils_common.py
@@ -19,3 +19,23 @@ np.set_printoptions(precision=2)
 dlc = dict(dlblue = '#0096ff', dlorange = '#FF9300', dldarkred='#C00000', dlmagenta='#FF40FF', dlpurple='#7030A0', dldarkblue =  '#0D5BDC')
 dlblue = '#0096ff'; dlorange = '#FF9300'; dldarkred='#C00000'; dlmagenta='#FF40FF'; dlpurple='#7030A0'; dldarkblue =  '#0D5BDC'
 dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+
+def sigmoid(z):
+   """
+   Compute the sigmoid of z
+
+   Parameters
+   ----------
+   z : array_like
+      A scalar or numpy array of any size.
+
+   Returns
+   -------
+   g : array_like
+         sigmoid(z)
+   """
+
+   z = np.clip(z, -500,500) #protect against overflow, no deja valores menores a -500 y mayores a 500 (los reemplaza)
+   g = 1.0/(1.0+np.exp(-z))
+   return g
+
