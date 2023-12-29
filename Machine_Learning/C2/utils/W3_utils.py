589a216927c48df3d1d647ab976d9d18a0663f5a
Matias Sanhueza <mvsanhueza@uc.cl>
2023-12-29 17:52:39 -0300

    C2_W3_L2

--- Machine_Learning/C2/utils/W3_utils.py
+++ Machine_Learning/C2/utils/W3_utils.py
@@ -93,3 +93,231 @@ def build_models():
     model_list = [model_1, model_2, model_3]
 
     return model_list
+
+def prepare_dataset(filename):
+
+    data = np.loadtxt(filename, delimiter=",")
+
+    x = data[:,:-1]
+    y = data[:,-1]
+
+    # Get 60% of the dataset as the training set. Put the remaining 40% in temporary variables.
+    x_train, x_, y_train, y_ = train_test_split(x, y, test_size=0.40, random_state=80)
+
+    #Split the 40% subset above into two: one half for cross validation and the other for the test set
+    x_cv, x_test, y_cv, y_test = train_test_split(x_, y_, test_size=0.50, random_state=80)
+
+    return x_train, y_train, x_cv, y_cv, x_test, y_test
+
+def train_plot_poly(model, x_train, y_train, x_cv, y_cv, max_degree=10, baseline=None):
+    
+    train_mses = []
+    cv_mses = []
+    models = []
+    scalers = []
+    degrees = range(1, max_degree+1)
+
+    # Loop over 10 times. Each adding one more degree of polynomial higher than the last
+    for degree in degrees:
+
+        #Add polynomial features to the training set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_train_mapped = poly.fit_transform(x_train)
+
+        #Scale the training set
+        scaler_poly = StandardScaler()
+        X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+        scalers.append(scaler_poly)
+
+        #Create and train the model
+        model.fit(X_train_mapped_scaled, y_train)
+        models.append(model)
+
+        # Compute the training MSE
+        yhat = model.predict(X_train_mapped_scaled)
+        train_mse = mean_squared_error(y_train, yhat) / 2.0
+        train_mses.append(train_mse)
+
+        # Add polynomial features and scale the cross-validation set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_cv_mapped = poly.fit_transform(x_cv)
+        X_train_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+        #Compute the cross validation MSE
+        yhat = model.predict(X_train_mapped_scaled)
+        cv_mse = mean_squared_error(y_cv, yhat) / 2.0
+        cv_mses.append(cv_mse)
+
+    #Plot the results
+    plt.plot(degrees, train_mses, marker='o', c='r', label='training MSEs'); 
+    plt.plot(degrees, cv_mses, marker='o', c='b', label='CV MSEs'); 
+    plt.plot(degrees, np.repeat(baseline, len(degrees)), linestyle='--', label='baseline')
+    plt.title("degree of polynomial vs. train and CV MSEs")
+    plt.xticks(degrees)
+    plt.xlabel("degree"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
+
+def train_plot_reg_params(reg_params, x_train, y_train, x_cv, y_cv, degree = 1, baseline=None):
+
+    train_mses = []
+    cv_mses = []
+    models = []
+    scalers = []
+
+    # Loop over 10 times. Each adding one more degree of polynomial higher than the last.
+    for reg_param in reg_params:
+
+        # Add polynomial features to the training set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_train_mapped = poly.fit_transform(x_train)
+
+        # Scale the training set
+        scaler_poly = StandardScaler()
+        X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+        scalers.append(scaler_poly)
+
+        # Create and train the model
+        model = Ridge(alpha=reg_param)
+        model.fit(X_train_mapped_scaled, y_train)
+        models.append(model)
+
+        # Compute the training MSE
+        yhat = model.predict(X_train_mapped_scaled)
+        train_mse = mean_squared_error(y_train, yhat) / 2
+        train_mses.append(train_mse)
+
+        # Add polynomial features and scale the cross-validation set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_cv_mapped = poly.fit_transform(x_cv)
+        X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+        # Compute the cross-validation MSE
+        yhat = model.predict(X_cv_mapped_scaled)
+        cv_mse = mean_squared_error(y_cv, yhat) / 2
+        cv_mses.append(cv_mse)
+
+    #Plot the results
+    reg_params = [str(x) for x in reg_params]
+    plt.plot(reg_params, train_mses, marker='o', c='r', label='training MSEs'); 
+    plt.plot(reg_params, cv_mses, marker='o', c='b', label='CV MSEs'); 
+    plt.plot(reg_params, np.repeat(baseline, len(reg_params)), linestyle='--', label='baseline')
+    plt.title("lambda vs. train and CV MSEs")
+    plt.xlabel("lambda"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
+
+def train_plot_diff_datasets(model, files, max_degree=10, baseline=None):
+    for file in files:
+        
+        x_train, y_train, x_cv, y_cv, x_test, y_test = prepare_dataset(file['filename'])
+
+        train_mses = []
+        cv_mses = []
+        models = []
+        scalers = []
+        degrees = range(1,max_degree+1)
+
+        # Loop over 10 times. Each adding one more degree of polynomial higher than the last.
+        for degree in degrees:
+
+            # Add polynomial features to the training set
+            poly = PolynomialFeatures(degree, include_bias=False)
+            X_train_mapped = poly.fit_transform(x_train)
+
+            # Scale the training set
+            scaler_poly = StandardScaler()
+            X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+            scalers.append(scaler_poly)
+
+            # Create and train the model
+            model.fit(X_train_mapped_scaled, y_train )
+            models.append(model)
+
+            # Compute the training MSE
+            yhat = model.predict(X_train_mapped_scaled)
+            train_mse = mean_squared_error(y_train, yhat) / 2
+            train_mses.append(train_mse)
+
+            # Add polynomial features and scale the cross-validation set
+            poly = PolynomialFeatures(degree, include_bias=False)
+            X_cv_mapped = poly.fit_transform(x_cv)
+            X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+            # Compute the cross-validation MSE
+            yhat = model.predict(X_cv_mapped_scaled)
+            cv_mse = mean_squared_error(y_cv, yhat) / 2
+            cv_mses.append(cv_mse)
+
+        # Plot the results
+        plt.plot(degrees, train_mses, marker='o', c='r', linestyle=file['linestyle'], label=f"{file['label']} training MSEs"); 
+        plt.plot(degrees, cv_mses, marker='o', c='b', linestyle=file['linestyle'], label=f"{file['label']} CV MSEs"); 
+
+    plt.plot(degrees, np.repeat(baseline, len(degrees)), linestyle='--', label='baseline')
+    plt.title("degree of polynomial vs. train and CV MSEs")
+    plt.xticks(degrees)
+    plt.xlabel("degree"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
+
+def train_plot_learning_curve(model, x_train, y_train, x_cv, y_cv, degree=1, baseline=None):
+
+    train_mses = []
+    cv_mses = []
+    models = []
+    scalers = []
+    num_samples_train_and_cv = []
+    percents = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+
+    # Loop over 10 times. Each adding one more degree of polynomial higher than the last.
+    for percent in percents:
+        
+        num_samples_train = round(len(x_train) * (percent/100.0))
+        num_samples_cv = round(len(x_cv) * (percent/100.0))
+        num_samples_train_and_cv.append(num_samples_train + num_samples_cv)
+        
+        x_train_sub = x_train[:num_samples_train]
+        y_train_sub = y_train[:num_samples_train]
+        x_cv_sub = x_cv[:num_samples_cv]
+        y_cv_sub = y_cv[:num_samples_cv]
+
+        # Add polynomial features to the training set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_train_mapped = poly.fit_transform(x_train_sub)
+
+        # Scale the training set
+        scaler_poly = StandardScaler()
+        X_train_mapped_scaled = scaler_poly.fit_transform(X_train_mapped)
+        scalers.append(scaler_poly)
+
+        # Create and train the model
+        model.fit(X_train_mapped_scaled, y_train_sub)
+        models.append(model)
+
+        # Compute the training MSE
+        yhat = model.predict(X_train_mapped_scaled)
+        train_mse = mean_squared_error(y_train_sub, yhat) / 2
+        train_mses.append(train_mse)
+
+        # Add polynomial features and scale the cross-validation set
+        poly = PolynomialFeatures(degree, include_bias=False)
+        X_cv_mapped = poly.fit_transform(x_cv_sub)
+        X_cv_mapped_scaled = scaler_poly.transform(X_cv_mapped)
+
+        # Compute the cross-validation MSE
+        yhat = model.predict(X_cv_mapped_scaled)
+        cv_mse = mean_squared_error(y_cv_sub, yhat) / 2
+        cv_mses.append(cv_mse)
+
+    # Plot the results
+    plt.plot(num_samples_train_and_cv, train_mses, marker='o', c='r', label='training MSEs'); 
+    plt.plot(num_samples_train_and_cv, cv_mses, marker='o', c='b', label='CV MSEs'); 
+    plt.plot(num_samples_train_and_cv, np.repeat(baseline, len(percents)), linestyle='--', label='baseline')
+    plt.title("number of examples vs. train and CV MSEs")
+    plt.xlabel("total number of training and cv examples"); 
+    plt.ylabel("MSE"); 
+    plt.legend()
+    plt.show()
